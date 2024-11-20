a464bf8b36e13d55ede923b40e0523f738d4f724
Callum Ilett(Front-End Developer) <cilett99@gmail.com>
2024-11-20 16:26:38 +0000

    ✨ Add key/value storage

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,39 +1,9 @@
-import React, { useState } from "react";
-import {
-	Button,
-	SafeAreaView,
-	StyleSheet,
-	Text,
-	TextInput
-} from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
-	const [taps, setTaps] = useState(0);
-	const [text, setText] = useState("");
 	return (
-		<SafeAreaView style={styles.container}>
-			<Button title="Add one" onPress={() => setTaps(taps + 1)} />
-			<Button
-				title="Add ten"
-				testID="add_ten"
-				onPress={() => setTaps(taps + 10)}
-			/>
-			<Text>Number of taps: {taps}</Text>
-			<TextInput
-				testID="text_input"
-				placeholder="Change me!"
-				onChangeText={setText}
-			/>
-			<Text>You typed: {text}</Text>
-		</SafeAreaView>
+		<View>
+			<Text>Welcome to React Native</Text>
+		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center"
-	}
-});
