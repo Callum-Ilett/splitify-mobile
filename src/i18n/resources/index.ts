9857e0ff8101ed7aa51d540940d39df3bc61b58f
Callum Ilett(Front-End Developer) <cilett99@gmail.com>
2024-11-20 10:34:46 +0000

    🌐 Add en, fr, de, es locale translations

---
+++ src/i18n/resources/index.ts
@@ -0,0 +1,15 @@
+import en from "./en/default.json";
+import es from "./es/default.json";
+import fr from "./fr/default.json";
+
+export const resources = {
+	en: {
+		translation: en
+	},
+	fr: {
+		translation: fr
+	},
+	es: {
+		translation: es
+	}
+};
