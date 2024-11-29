9ccd139c316b4b55bb2f8388f760504ae62b19fe
Callum Ilett(Front-End Developer) <cilett99@gmail.com>
2024-11-29 20:55:17 +0000

    ✨ Add VerifyPhoneNumberScreen component with unit tests
    - Introduced the VerifyPhoneNumberScreen component for phone number verification, allowing users to enter their phone number and receive an SMS code.
    - Implemented form submission handling with validation and navigation to the VerifyCodeScreen upon successful SMS code sending.
    - Created unit tests for VerifyPhoneNumberScreen to verify rendering, heading display, and form submission behavior.
    - Added an index file for exporting the VerifyPhoneNumberScreen component.

---
+++ src/features/auth/screens/VerifyPhoneNumber/VerifyPhoneNumberScreen.tsx
@@ -0,0 +1,62 @@
+import type { Schema as VerifyPhoneNumberFormSchema } from "@/features/auth/components/VerifyPhoneNumberForm";
+
+import { H3 } from "@/components/ui/Heading";
+import { Text } from "@/components/ui/Text";
+import { VerifyPhoneNumberForm } from "@/features/auth/components/VerifyPhoneNumberForm";
+import { useAuth } from "@/features/auth/hooks/useAuth";
+import { formatPhoneNumber } from "@/features/auth/utils";
+import { useRouter } from "expo-router";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+type VerifyPhoneNumberScreenProps = {
+	heading: string;
+};
+
+const VerifyPhoneNumberScreen = ({ heading }: VerifyPhoneNumberScreenProps) => {
+	const router = useRouter();
+	const { sendSMSCode } = useAuth();
+
+	const onSubmit = async (data: VerifyPhoneNumberFormSchema) => {
+		const { callingCode, phoneNumber } = data;
+
+		const formattedPhoneNumber = formatPhoneNumber({
+			callingCode,
+			phoneNumber
+		});
+
+		try {
+			console.log("Sending SMS code to phone number:", formattedPhoneNumber);
+			await sendSMSCode({
+				phoneNumber: formattedPhoneNumber,
+				send: "code"
+			});
+
+			router.push({
+				pathname: "/verify-code",
+				params: { phoneNumber: formattedPhoneNumber }
+			});
+		} catch {
+			console.error("Error sending SMS code");
+		}
+	};
+
+	return (
+		<SafeAreaView className="flex-1 px-6 py-3" edges={["bottom"]}>
+			<View className="gap-4">
+				<H3>{heading}</H3>
+
+				<Text className="text-xl">
+					Enter your phone number below. A SMS will be sent to that number with
+					a OTP code.
+				</Text>
+			</View>
+
+			<VerifyPhoneNumberForm className="mt-8 flex-1" onSubmit={onSubmit} />
+		</SafeAreaView>
+	);
+};
+VerifyPhoneNumberScreen.displayName = "VerifyPhoneNumberScreen";
+
+export type { VerifyPhoneNumberScreenProps };
+export { VerifyPhoneNumberScreen };
